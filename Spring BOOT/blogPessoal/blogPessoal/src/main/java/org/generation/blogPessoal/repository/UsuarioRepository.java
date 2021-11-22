5da593509054d34947855a276478afcf322ed66c
MariaLuizaDMoura <malumoura10@gmail.com>
2021-11-22 16:49:53 -0300

    JUnit

--- Spring BOOT/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ Spring BOOT/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package org.generation.blogPessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.blogPessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-	public Optional<Usuario> findByUsuario(String usuario);
+	public Usuario findByUsuario(String usuario);
+
+	public List<Usuario> findAllByUsuarioContainingIgnoreCase(String string);
 }
